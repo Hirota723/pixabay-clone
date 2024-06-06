e2c44f3de3c252503e5d943f8e8471b0059af3ab
ひろ <koki.hiro0723@gmail.com>
2024-06-06 21:52:03 +0900

    ♻️ リファクタリングした

--- src/pages/component/ImageGallery.tsx
+++ src/pages/component/ImageGallery.tsx
@@ -13,11 +13,11 @@ const ImageGallery: React.FC<ImageGalleryProps> = ({ fetchData }) => {
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
       {fetchData.map((data) => (
         <div key={data.id} className="w-64 h-64">
-          <a href={data.pageURL} target="_blank" rel="noopener noreferrer">
+          <a href={data.pageURL} target="_blank">
             <img
-              className="w-full h-full object-cover rounded shadow-md bg-center cursor-pointer hover:shadow-none"
               src={data.largeImageURL}
-              alt={`Image from ${data.pageURL}`}
+              alt={data.pageURL}
+              className="w-full h-full object-cover rounded shadow-md bg-center cursor-pointer hover:shadow-none"
             />
           </a>
         </div>
--- src/pages/component/SearchForm.tsx
+++ src/pages/component/SearchForm.tsx
@@ -7,23 +7,17 @@ interface SearchFormProps {
 const SearchForm: React.FC<SearchFormProps> = ({ setFetchData }) => {
   const ref = useRef<HTMLInputElement>(null);
 
-  const handleSubmit = async (e: React.FormEvent) => {
+  const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
 
     const apiKey = "44252574-0f66666e1bb0e41e2fc432aa5";
-    const query = ref.current?.value;
-    if (!apiKey || !query) return;
+    const endpointURL = `https://pixabay.com/api/?key=${apiKey}&q=${ref.current?.value}&image_type=photo`;
 
-    const endpointURL = `https://pixabay.com/api/?key=${apiKey}&q=${query}&image_type=photo`;
-
-    try {
-      const response = await fetch(endpointURL);
-      if (!response.ok) throw new Error("Network response was not ok");
-      const data = await response.json();
-      setFetchData(data.hits);
-    } catch (error) {
-      console.error("Fetch error: ", error);
-    }
+    fetch(endpointURL)
+      .then((res) => res.json())
+      .then((data) => {
+        setFetchData(data.hits);
+      });
   };
 
   return (
