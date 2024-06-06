d7a993a9d97c40c1ba7a4aff8fface45bbde10f7
ひろ <koki.hiro0723@gmail.com>
2024-06-06 20:34:23 +0900

    ✨ 画像検索機能の追加

--- src/pages/component/ImageGallery.tsx
+++ src/pages/component/ImageGallery.tsx
@@ -1,7 +1,29 @@
 import React from "react";
 
-const ImageGallery = () => {
-  return <div>ImageGallery</div>;
+interface ImageGalleryProps {
+  fetchData: {
+    id: number;
+    pageURL: string;
+    largeImageURL: string;
+  }[];
+}
+
+const ImageGallery: React.FC<ImageGalleryProps> = ({ fetchData }) => {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
+      {fetchData.map((data) => (
+        <div key={data.id} className="w-64 h-64">
+          <a href={data.pageURL} target="_blank">
+            <img
+              className="w-full h-full object-cover rounded shadow-md bg-center cursor-pointer hover:shadow-none"
+              src={data.largeImageURL}
+              alt=""
+            />
+          </a>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default ImageGallery;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,21 +1,42 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import ImageGallery from "./component/ImageGallery";
+import { useRef, useState } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 const Home = () => {
+  const [fetchData, setFetchData] = useState([]);
+  const ref = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(ref.current?.value);
+
+    const endpointURL = `https://pixabay.com/api/?key=44252574-0f66666e1bb0e41e2fc432aa5&q=${ref.current?.value}&image_type=photo`;
+
+    fetch(endpointURL)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data.hits);
+        setFetchData(data.hits);
+      });
+  };
+
   return (
-    <main className="flex flex-col items-center w-full h-screen bg-gray-200">
+    <main className="flex flex-col items-center w-full min-h-screen bg-gray-200">
       <h2 className="text-4xl font-bold text-gray-700 pt-10">My Pixabay</h2>
-      <form className="w-full max-w-xl m-5">
+      <form onSubmit={(e) => handleSubmit(e)} className="w-full max-w-xl m-5">
         <input
           type="text"
           placeholder="画像を探す"
+          ref={ref}
           className="w-full outline-none px-2 py-2 bg-white shadow-md rounded-lg"
         />
       </form>
-      <ImageGallery />
+      <ImageGallery fetchData={fetchData} />
     </main>
   );
 };
